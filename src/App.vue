3fa41d517a1316653b5c840cdff3976a47d86abb
salekin01 <salekin01@outlook.com>
2021-02-06 22:38:05 +0100

    static sidebar and navbar, icon added (npm install --save @mdi/js)

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <!--side navbar-->
+        <v-navigation-drawer v-model="sideNav" temporary>
+            <v-list>
+                <v-list-item-group v-model="group">
+                    <v-list-item>
+                        <v-icon role="img">mdi-pizza</v-icon>
+                        Pizza
+                    </v-list-item>
+                </v-list-item-group>
+                <v-list-item-group>
+                    <v-list-item>
+                        <v-icon role="img">mdi-dice-5</v-icon>
+                        Ingredient
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
 
-      <v-spacer></v-spacer>
+        <!--navbar-->
+        <v-app-bar class="red" app>
+            <v-app-bar-nav-icon @click="sideNav = true" class="hidden-sm-and-up">
+                <v-icon role="img">mdi-menu</v-icon>
+            </v-app-bar-nav-icon>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <v-toolbar-title>PizzaPoint</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn>
+                    <v-icon role="img">mdi-pizza</v-icon>
+                    Pizza
+                </v-btn>
+                <v-btn>
+                    <v-icon role="img">mdi-dice-5</v-icon>
+                    Ingredient
+                </v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <main>
+
+        </main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+    //import HelloWorld from './components/HelloWorld';
+    import {mdiMenu} from '@mdi/js';
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+        components: {
+            //HelloWorld,
+        },
 
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+            sideNav: false,
+            icon: {
+                mdiMenu
+            },
+            group: null
+        }),
+    };
 </script>
+
+
